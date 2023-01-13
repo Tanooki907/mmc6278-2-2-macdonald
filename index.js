b5fb5331b38d992abba5aae2ee1c8b872b35c678
Nicholas MacDonald <macdonaldn@ufl.edu>
2023-01-13 14:26:36 -0500

    build quote fetcher

--- index.js
+++ index.js
@@ -13,6 +13,12 @@ program
   .description("Retrieves a random quote")
   .action(async () => {
     // TODO: Pull a random quote from the quotes.txt file
+    const fs = require('fs')
+    fs.readFile(QUOTE_FILE, 'utf-8', function(err, data) {
+      if (err) throw err
+      var QuoteList = data.split('\n')
+      console.log(QuoteList[Math.floor(Math.random()*QuoteList.length)].split('|'))
+    })
     // console log the quote and author
     // You may style the text with chalk as you wish
   });
@@ -22,6 +28,13 @@ program
   .description("adds a quote to the quote file")
   .action(async (quote, author) => {
     // TODO: Add the quote and author to the quotes.txt file
+    if (!author){
+      author = 'anonymous'
+    }
+    fs.appendFile(QUOTE_FILE, quote + '|' + author + '\n', function (err) {
+      if (err) throw err
+    })
+    console.log("Updated!")
     // If no author is provided,
     // save the author as "Anonymous".
     // After the quote/author is saved,
